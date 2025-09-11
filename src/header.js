f63099c3214f9715e71409dcaaae4795e8e2bb54
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-09-11 11:49:56 -0400

    Added html loader to process images added some images created seperate section for footer and header and added some styles

---
+++ src/header.js
@@ -0,0 +1,30 @@
+import "./style.css"
+import logo from "./assets/logo.png"
+
+// The necessary dom elements
+const header = document.createElement("div");
+const toolbar = document.createElement("div");
+const navBar = document.createElement("div");
+const logoText = document.createElement("div");
+const image = document.createElement("img");
+
+// Adding Id's
+header.id = "header";
+toolbar.id = "toolBar-header";
+navBar.id = "navBar-header";
+
+// Tool Bar sectiorn
+
+//Nav Bar section
+
+// Logo and text
+const titleText = document.createElement("")
+image.src = logo
+navBar.appendChild(image)
+
+
+
+
+
+
+export { header }
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "./style.css"
+import { header } from "./header";
+// Creating sections
+const body = document.querySelector("body");
+const container = document.createElement("div");
+
+
+
+// Adding Id's to our main sections
+container.id = "container";
+
+// addind our main dections to the dom
+container.appendChild(header)
+body.appendChild(container)
+
+
+
+
+
