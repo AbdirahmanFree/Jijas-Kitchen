d7ad56702e52c1ed4c36c0055991105f28734b3c
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-09-13 11:35:19 -0400

    Added Images of food

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import { footer } from "./footer";
 // Creating sections
 const body = document.querySelector("body");
 const container = document.createElement("div");
+const content = document.createElement("div");
 
 
 
 // Adding Id's to our main sections
 container.id = "container";
+content.id = "content"
 
 // addind our main dections to the dom
 container.appendChild(header)
+container.appendChild(content)
 container.appendChild(footer)
 body.appendChild(container)
 
